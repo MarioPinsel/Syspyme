ea561335ec20d9d4ca8bfa8c18d5c1e770bcc541
ElRam3n <ramgosantiti@gmail.com>
2025-10-13 19:33:40 -0500

    Login
    El Header y el Login estan hechos, cualquier cosa al nequi

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,11 +1,26 @@
 import './Header.css';
+import { FaUser, FaRightToBracket } from "react-icons/fa6";
 
 export default function Header() {
 
     return (
         <>
-            <header>
-
+            <header className="barranav">
+                <div className="left">
+                    <div className="logo">
+                        <img src="/Logo S.png" alt="Logo S" />
+                    </div>
+                    <div className="sep"/>
+                    <nav className="navegacion">
+                        <a href="#">INICIO</a>
+                        <a href="#">PLANES</a>
+                        <a href="#">ABOUT US</a>
+                    </nav>
+                </div>
+                <div className="iconos">
+                    <FaUser />
+                    <FaRightToBracket />
+                </div>
             </header>
         </>
     )
--- Frontend/src/views/LoginView.jsx
+++ Frontend/src/views/LoginView.jsx
@@ -1,8 +1,31 @@
-export default function LoginView() {
-    return (
-        <>
-            <h1>Login</h1>
-        </>
-    )
+import './LoginView.css';
+
+
+function LoginView() {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Crea una cuenta</h2>
+        <form>
+          <label>Nombre</label>
+          <input type="text" placeholder="Ingresa tu Nombre" />
+
+          <label>Usuario</label>
+          <input type="text" placeholder="Ingresa tu Usuario" />
+
+          <label>Email</label>
+          <input type="text" placeholder="Ingresa tu Email" />
+
+          <label>Contraseña</label>
+          <input type="password" placeholder="Ingresa tu Contraseña" />
+
+          <button type="submit">Registrarse</button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default LoginView;
+
 
