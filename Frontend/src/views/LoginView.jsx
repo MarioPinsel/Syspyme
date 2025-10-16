9cc0d222c7d326588f1f8aa85ad30bf363694b38
ElRam3n <ramgosantiti@gmail.com>
2025-10-15 19:04:08 -0500

    Register con Register y Login con Login
    Decidí hacer un RegisterView.css copiando el estilo en vez de hacer globales porque tocaba cambiar mucho la estructura, ya eso prefiero mirarlo con mario en vez de mover de mas, los tqm

--- Frontend/src/views/LoginView.jsx
+++ Frontend/src/views/LoginView.jsx
@@ -5,21 +5,15 @@ function LoginView() {
   return (
     <div className="login-container">
       <div className="login-box">
-        <h2>Crea una cuenta</h2>
+        <h2>Iniciar Sesión</h2>
         <form>
-          <label>Nombre</label>
-          <input type="text" placeholder="Ingresa tu Nombre" />
-
           <label>Usuario</label>
           <input type="text" placeholder="Ingresa tu Usuario" />
-
-          <label>Email</label>
-          <input type="text" placeholder="Ingresa tu Email" />
 
           <label>Contraseña</label>
           <input type="password" placeholder="Ingresa tu Contraseña" />
 
-          <button type="submit">Registrarse</button>
+          <button type="submit">Continuar</button>
         </form>
       </div>
     </div>
--- Frontend/src/views/RegisterView.jsx
+++ Frontend/src/views/RegisterView.jsx
@@ -1,5 +1,31 @@
-export default function RegisterView() {
-    return (
-        <div>RegisterView</div>
-    )
+import './RegisterView.css';
+
+
+function RegisterView() {
+  return (
+    <div className="register-container">
+      <div className="register-box">
+        <h2>Crea tu cuenta</h2>
+        <form>
+          <label>Usuario</label>
+          <input type="text" placeholder="Ingresa tu Usuario" />
+
+            <label>Nombre</label>
+          <input type="text" placeholder="Ingresa tu Nombre" />
+          
+          <label>Email</label>
+          <input type="text" placeholder="Ingresa tu Email" />
+
+          <label>Contraseña</label>
+          <input type="password" placeholder="Ingresa tu Contraseña" />
+
+        
+
+          <button type="submit">Registrarse</button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default RegisterView;
