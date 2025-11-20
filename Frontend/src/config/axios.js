c80f3b6c7ed9b1eb9301a8b4b7b0121e004a4b25
Mariopinsel <pmlaser25@gmail.com>
2025-11-20 01:05:48 -0500

    por favor

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -3,9 +3,6 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
-if (baseURL != import.meta.env.VITE_API_URL) {
-  console.error("VITE_API_URL is not defined");
-}
 
 
 export default api;
