e47dc9691afdc4603388f08f1b43b20f727d85f8
Fab1annnnnn <fabiandresbaron@gmail.com>
2025-11-23 16:18:18 -0500

    inventario respuestas

--- Backend/src/controllers/inventoryController.js
+++ Backend/src/controllers/inventoryController.js
@@ -1,74 +1,62 @@
-import { createProductService, addProductService, updateProductService, getProductsService, deleteProductService } from "../services/inventoryService.js"
+import {
+    createProductService,
+    addProductService,
+    updateProductService,
+    getProductsService,
+    deleteProductService
+} from "../services/inventoryService.js";
 
 export const createProductController = async (req, res) => {
     try {
         const pool = req.pool;
         const result = await createProductService(pool, req.body);
-
-        if (!result.success) {
-            return res.status(409).json(result);
-        }
-
-        res.status(201).json(result);
+        return res.status(result.status).json(result);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ success: false, message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 };
 
 export const addProductController = async (req, res) => {
     try {
         const pool = req.pool;
-        const result = await addProductService(pool, req.body)
-        res.status(201).json(result)
+        const result = await addProductService(pool, req.body);
+        return res.status(result.status).json(result);
     } catch (error) {
-        if (error.message === 'EMPTY') return res.status(409).json({ error: 'No existe ese producto con ese id o con ese codigo' });
         console.error(error);
-        res.status(500).json({ error: error.message });
+        return res.status(500).json({ message: error.message });
     }
-}
+};
 
 export const updateProductController = async (req, res) => {
     try {
         const pool = req.pool;
         const result = await updateProductService(pool, req.body);
-
-        if (result.success === false) {
-            return res.status(409).json(result);
-        }
-
-        return res.status(200).json(result);
-
+        return res.status(result.status).json(result);
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ error: error.message });
+        return res.status(500).json({ message: error.message });
     }
 };
-
 
 export const getProductsController = async (req, res) => {
     try {
         const pool = req.pool;
-        const result = await getProductsService(pool, req.query)
-        if (result.success === false) {
-            return res.status(409).json(result);
-        }
-        res.status(200).json(result)
+        const result = await getProductsService(pool, req.query);
+        return res.status(result.status).json(result);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: error.message });
+        return res.status(500).json({ message: error.message });
     }
-}
+};
 
 export const deleteProductController = async (req, res) => {
     try {
         const pool = req.pool;
-        const result = await deleteProductService(pool, req.body)
-        res.status(200).json(result)
+        const result = await deleteProductService(pool, req.body);
+        return res.status(result.status).json(result);
     } catch (error) {
-        if (error.message === 'NOT_FOUND_INVENTORY') return res.status(404).json({ error: 'No hay productos en el inventario pa eliminar.' });
-        if (error.message === 'NOT_FOUND') return res.status(404).json({ error: 'No existe el producto.' });
         console.error(error);
-        res.status(500).json({ error: error.message });
+        return res.status(500).json({ message: error.message });
     }
-}
+};
