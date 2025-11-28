2e81f24da7979524d0a1086ad0e93e91ee6e4570
Fab1annnnnn <fabiandresbaron@gmail.com>
2025-11-28 16:14:14 -0500

    arreglar buscar por usuario

--- Backend/src/repositories/userDIAN/userDIANRepository.js
+++ Backend/src/repositories/userDIAN/userDIANRepository.js
@@ -1,6 +1,6 @@
 import { pool } from '../../config/conectionCore.js';
 
 export const findUserDIANByUsuario = async (usuario) => {
-    const query = `SELECT * FROM usuariosdian WHERE usuario = $1`;
+    const query = `SELECT * FROM usuariosdian WHERE nombre = $1`;
     return await pool.query(query, [usuario]);
 }
